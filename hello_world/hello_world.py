182e8f2dff1f31bde2ea60ec38349ed14de99686
Ivan Vazquez <ivan@ivanvazquez.com>
2012-01-18 19:15:43 -0700

    added!

---
+++ hello_world/hello_world.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+class MyProgram:
+    message = "Foo You"
+    def __init__ (self):
+        self.message = "Hello World"
+
+    def print_message (self):
+        print self.message
+        print MyProgram.message
+
+            
+        
+if __name__ == "__main__":
+    program = MyProgram()
+    program.print_message()
