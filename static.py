9c74db9d0230a4da60013764b91127f4829e8bb9
Ivan Vazquez <ivan@smtp.fooz.lan>
2013-09-06 22:47:11 -0700

    Added use of super for classmethod example

--- static.py
+++ static.py
@@ -1,5 +1,7 @@
 
 class MyClass():
+    x_factor = 8
+
     @staticmethod
     def do_thing(x):
         return x+1
@@ -8,3 +10,17 @@ class MyClass():
     @staticmethod
     def do_other_thing(y):
         return MyClass.do_thing(y + 1)
+
+    @classmethod
+    def ya_thing(cls, x):
+        return x + 10 + cls.x_factor
+
+
+
+class SubClass(MyClass):
+    x_factor = 1000
+
+    @classmethod
+    def ya_thing(cls, x):
+        ox = super().ya_thing(x)
+        return ox + 100 + cls.x_factor
