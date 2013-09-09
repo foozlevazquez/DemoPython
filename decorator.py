69c9c49536d5a6a29a226a50b22a914e72cddc8e
Ivan Vazquez <ivan@serverpilot.io>
2013-09-08 22:12:30 -0700

    Added crazy arguments and class methods.

--- decorator.py
+++ decorator.py
@@ -1,5 +1,6 @@
 import functools
 
+
 class DaClass():
 
     def __init__(self, got_thing):
@@ -12,6 +13,8 @@ class DaClass():
             return func(self, *args, **kw)
         return functools.update_wrapper(check_for_thing, func)
 
+
+
     @has_thing
     def must_have_thing(self):
         return True
@@ -19,7 +22,36 @@ class DaClass():
     def dont_care_bout_things(self):
         return True
 
+
+def wrap_func(func=None, other="yes"):
+    if func:
+        def wrapper(cls, *args, **kwargs):
+            print("cls = {}, func= {}, other={}".format(cls, func, other))
+            func(cls, *args, **kwargs)
+    else:
+        def wrapper(func):
+            return wrap_func(func=func, other=other)
+
+    return wrapper
+
+
+class SubClass(DaClass):
+    @classmethod
+    @wrap_func()
+    def foo(cls):
+        print("In foo")
+
+class YaClass():
+        @classmethod
+        @wrap_func(other="no")
+        def bar(cls):
+            print("In bar")
+
 if __name__ == '__main__':
+
+    SubClass.foo()
+    YaClass.bar()
+
     yes = DaClass(True)
     no = DaClass(False)
 
