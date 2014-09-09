81b8893f345303d95cbcbc6adc9aa01e7b3095ef
Ivan Vazquez <ivan@ivanvazquez.com>
2014-09-08 21:56:02 -0700

    Shows breakage.

--- static_classes_and_process_safety/unsafe_static.py
+++ static_classes_and_process_safety/unsafe_static.py
@@ -9,11 +9,29 @@ class UnsafeStaticClass:
     @classmethod
     def use(cls):
         calling_process = multiprocessing.current_process()
-
-        if bff_process:
+        print("calling process = {}".format(calling_process))
+        if cls.bff_process:
             if calling_process is not cls.bff_process:
                 raise Exception("kaboom.")
         else:
+            print("bff process = {}".format(calling_process))
             cls.bff_process = calling_process
 
         return True
+
+
+class SubProcess(multiprocessing.Process):
+    def run(self):
+        UnsafeStaticClass.use()
+        print("SubProcess Done")
+
+def main():
+    UnsafeStaticClass.use()
+    sp = SubProcess()
+    sp.start()
+    sp.join()
+    print("All Done")
+
+
+if __name__ == '__main__':
+    main()
